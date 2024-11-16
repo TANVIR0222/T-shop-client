e6fc1792700756f284b80400bde53101f9d59f5a
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-11-16 15:46:51 +0600

    add link and view card details

--- src/components/common/CollectionCart.jsx
+++ src/components/common/CollectionCart.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const CollectionCart = ({item}) => {
     return (
         <div>
+            <Link to={`/product/${item._id}`}>
             <div key={item._id} className=" rounded-sm space-y-3 shadow p-2  h-[420px] ">
                 <img className='h-60 w-full px-2 md:px-10' src={item.image[0]} alt="" />
                 <h1>{item.name}</h1>
@@ -12,6 +14,7 @@ const CollectionCart = ({item}) => {
                 </div>
                 <p>{item.description.slice(0,50)}...</p>
             </div> 
+            </Link>
         </div>
     );
 };
