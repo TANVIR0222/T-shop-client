2f9a678286c6f6a611096d301529993b44a0a8c4
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-11-16 15:42:15 +0600

    add react toast

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,15 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Router/Router";
 
+
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
 createRoot(document.getElementById("root")).render(
   <div className="max-w-screen-xl mx-auto	">
     <StrictMode>
+    <ToastContainer />
       <RouterProvider router={router} />
     </StrictMode>
   </div>
