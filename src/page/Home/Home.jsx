29f7e48c1a0123c0c939a10f8967890c89c93814
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-11-16 15:51:10 +0600

    Home

---
+++ src/page/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Hero from "./Hero";
+import ShowProduct from "./ShowProduct";
+
+const Home = () => {
+    return (
+        <div>
+           <Hero /> 
+           <ShowProduct />
+        </div>
+    );
+};
+
+export default Home;
