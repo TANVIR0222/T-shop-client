1bef8a9167b77a7cd6e621316f8dac5f83441ed8
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-12-01 20:51:10 +0600

    Router

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,8 +1,13 @@
 import App from "@/App";
 import About from "@/page/About/About";
+import Login from "@/page/auth/Login";
 import Collection from "@/page/Collection/Collection";
 import Home from "@/page/Home/Home";
+import Order from "@/page/order/Order";
+import PlaceOrder from "@/page/order/PlaceOrder";
+import CartView from "@/page/view/CartView";
 import ProductView from "@/page/view/ProductView";
+import AddToCart from "@/utils/addToCart";
 import {
     createBrowserRouter,
   } from "react-router-dom";
@@ -27,6 +32,22 @@ export  const router = createBrowserRouter([
         {
           path: "/product/:id",
           element: <ProductView />,
+        },
+        {
+          path: "/adProduct",
+          element: <CartView />,
+        },
+        {
+          path: "/placeOrder",
+          element: <PlaceOrder />,
+        },
+        {
+          path: "/order",
+          element: <Order />,
+        },
+        {
+          path: "/login",
+          element: <Login />,
         },
         
       ]
