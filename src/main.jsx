f1e2ee0b24f40a1dba884f461fcf9f7a78f99c64
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-12-01 20:50:18 +0600

    redux store

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,18 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Router/Router";
 
-
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 
 createRoot(document.getElementById("root")).render(
   <div className="max-w-screen-xl mx-auto	">
     <StrictMode>
-    <ToastContainer />
-      <RouterProvider router={router} />
+      <Provider store={store}>
+        <ToastContainer />
+        <RouterProvider router={router} />
+      </Provider>
     </StrictMode>
   </div>
 );
