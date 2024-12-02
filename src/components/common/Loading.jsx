3cc8c6cb1ec77b1d1c676873992b777c915a97d5
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-12-02 21:38:50 +0600

    Loading

---
+++ src/components/common/Loading.jsx
@@ -0,0 +1,10 @@
+
+const Loading = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="w-12 h-12 border-b-2 border-gray-900 border-solid rounded-full animate-spin"></div>
+    </div>
+  );
+};
+
+export default Loading;
