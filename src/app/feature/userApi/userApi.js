aca6897d85fc80ac68162251ccd00abaaaa5042a
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-12-03 23:39:07 +0600

    user Api

--- src/app/feature/userApi/userApi.js
+++ src/app/feature/userApi/userApi.js
@@ -4,11 +4,37 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const userApi = createApi({
   reducerPath: "userApi",
   baseQuery: fetchBaseQuery({ baseUrl: `${getBaseUrl()}/user/` }),
+  tagTypes: ["User"],
   endpoints: (builder) => ({
     fetchSingleUser: builder.query({
       query: (id) => `single-user/${id}`,
+      providesTags: ["User"],
+    }),
+    fetchAllUser: builder.query({
+      query: () => `all-user`,
+      providesTags: ["User"],
+    }),
+    updateUserRole: builder.mutation({
+      query: ({ id, ...res }) => ({
+        url: `update-role/${id}`,
+        method: "PATCH",
+        body: res,
+      }),
+      invalidatesTags: ["User"],
+    }),
+    deleteSingleUser: builder.mutation({
+      query: (id) => ({
+        url: `delete-user/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["User"],
     }),
   }),
 });
 
-export const {useFetchSingleUserQuery} = userApi;
+export const {
+  useFetchSingleUserQuery,
+  useFetchAllUserQuery,
+  useUpdateUserRoleMutation,
+  useDeleteSingleUserMutation,
+} = userApi;
