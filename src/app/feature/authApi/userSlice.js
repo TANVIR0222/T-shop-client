74aa61522ed28ad7f6db9f9ab6d28612579f0414
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-12-03 10:35:31 +0600

    user slice

--- src/app/feature/authApi/userSlice.js
+++ src/app/feature/authApi/userSlice.js
@@ -1,22 +1,36 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
-  user: null,
+const loadUserFormLocalStorage = () => {
+  try {
+    const serializedState = localStorage.getItem("user");
+    if (serializedState === null) return { user: null };
+
+    return { user: JSON.parse(serializedState) };
+  } catch (error) {
+    return { user: null };
+  }
 };
 
-export const userSlice = createSlice({
-  name: "user",
+const initialState = loadUserFormLocalStorage();
+
+export const authSlice = createSlice({
+  name: "authSlice",
   initialState,
   reducers: {
     setUser: (state, action) => {
-      state.user = action.payload;
-      console.log(state.user);
+      state.user = action.payload;      
+      localStorage.setItem("user", JSON.stringify(state.user));
     },
-
-    logout: (state, action) => {},
+    logout: (state) => {
+      state.user = null;
+      localStorage.removeItem("user");
+      localStorage.removeItem("token");
+    },
+    
   },
 });
 
-export const { setUser, logout } = userSlice.actions;
+// Action creators are generated for each case reducer function
+export const { setUser, logout , setListing} = authSlice.actions;
 
-export default userSlice.reducer;
+export default authSlice.reducer;
