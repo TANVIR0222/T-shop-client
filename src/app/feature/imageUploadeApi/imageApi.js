13bbaefb1d57fcbbbee69ad132891b30dddcb84a
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-12-03 12:25:34 +0600

    image Api

---
+++ src/app/feature/imageUploadeApi/imageApi.js
@@ -0,0 +1,18 @@
+import getBaseUrl from "@/utils/getBaseUrl";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const imageApi = createApi({
+  reducerPath: "imageApi",
+  baseQuery: fetchBaseQuery({ baseUrl: `${getBaseUrl()}/image/` }),
+  endpoints: (builder) => ({
+    imageUploade: builder.mutation({
+      query: (formData) => ({
+        url: `/uploade`,
+        method: "POST",
+        body: formData
+      })
+    }),
+  }),
+});
+
+export const {useImageUploadeMutation} = imageApi;
--- src/app/store.js
+++ src/app/store.js
@@ -4,6 +4,7 @@ import  cartReducer  from './feature/cart/cartSlice'
 import { authApi } from './feature/authApi/AuthApi'
 import  userReducer  from './feature/authApi/userSlice'
 import { userApi } from './feature/userApi/userApi'
+import { imageApi } from './feature/imageUploadeApi/imageApi'
 
 export const store = configureStore({
   reducer: {
@@ -11,10 +12,11 @@ export const store = configureStore({
     auth: userReducer,
     [authApi.reducerPath]: authApi.reducer,
     [userApi.reducerPath]: userApi.reducer,
+    [imageApi.reducerPath]: imageApi.reducer,
   },
   
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(authApi.middleware, userApi.middleware),
+    getDefaultMiddleware().concat(authApi.middleware, userApi.middleware, imageApi.middleware),
 })
 
 setupListeners(store.dispatch)
