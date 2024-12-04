d1fd5bb3bb7833ff6dea612c81ab4452d91fc680
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-12-04 16:26:56 +0600

    router

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -38,7 +38,7 @@ export  const router = createBrowserRouter([
           element: <ProductView />,
         },
         {
-          path: "/adProduct",
+          path: "/add-order",
           element: <CartView />,
         },
         {
