e59765a57dbb7b2f9c273c065d3b9c1ad8272de6
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-12-07 22:43:33 +0600

    update User Dropdown Menu

--- src/components/common/UserDropdownMenu.jsx
+++ src/components/common/UserDropdownMenu.jsx
@@ -1,3 +1,4 @@
+import { logout } from "@/app/feature/authApi/userSlice";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -6,32 +7,55 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 
-const UserDropdownMenu = ({ user }) => {
-  const {  role,avatar } = user?.user;
+const UserDropdownMenu = ({ user, role }) => {
+  const dispatch = useDispatch();
+  const handleLogout = async () => {
+    try {
+      await dispatch(logout());
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div>
       <DropdownMenu>
         <DropdownMenuTrigger>
-          <img className="w-10 h-10 rounded-full" src={avatar} alt="" />
+          <img className="w-10 h-10 rounded-full" src={user?.avatar} alt="" />
         </DropdownMenuTrigger>
 
         {role === "USER" ? (
           <DropdownMenuContent>
             <DropdownMenuLabel>My Account ({role}) </DropdownMenuLabel>
             <DropdownMenuSeparator />
-            <Link to={"/order"}><DropdownMenuItem>My Order</DropdownMenuItem></Link>
-            <Link><DropdownMenuItem>Payment History</DropdownMenuItem>{" "}</Link>
+            <Link to={"/order"}>
+              <DropdownMenuItem>My Order</DropdownMenuItem>
+            </Link>
+            <Link>
+              <DropdownMenuItem>Payment History</DropdownMenuItem>{" "}
+            </Link>
+            <DropdownMenuItem onClick={handleLogout}>Logout</DropdownMenuItem>
           </DropdownMenuContent>
         ) : (
           <DropdownMenuContent>
             <DropdownMenuLabel>My Account ({role}) </DropdownMenuLabel>
             <DropdownMenuSeparator />
-            <Link to={"/order"}><DropdownMenuItem>Add Product</DropdownMenuItem></Link>
-            <Link><DropdownMenuItem>All Product</DropdownMenuItem>{" "}</Link>
-            <Link><DropdownMenuItem>All Order</DropdownMenuItem>{" "}</Link>
+
+            <Link to={"/all-Order"}>
+              <DropdownMenuItem>All Order</DropdownMenuItem>{" "}
+            </Link>
+            <Link to={"/add-product"}>
+              <DropdownMenuItem>Add Product</DropdownMenuItem>{" "}
+            </Link>
+            <Link to={"/all-product"}>
+              <DropdownMenuItem>All Product</DropdownMenuItem>{" "}
+            </Link>
+            <Link to={"/all-user"}>
+              <DropdownMenuItem>All User</DropdownMenuItem>
+            </Link>
           </DropdownMenuContent>
         )}
       </DropdownMenu>
