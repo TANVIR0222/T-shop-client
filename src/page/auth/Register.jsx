16234b5e8902910649dda5a56ac089503bbae6eb
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-12-08 17:15:46 +0600

    Register page

--- src/page/auth/Register.jsx
+++ src/page/auth/Register.jsx
@@ -1,6 +1,7 @@
 import { useUserRegisterMutation } from "@/app/feature/authApi/AuthApi";
-import Loading from "@/components/common/Loading";
-import React, { useState } from "react";
+import { useImageUploadeMutation } from "@/app/feature/imageUploadeApi/imageApi";
+import ImageLoading from "@/components/common/ImageLoading";
+import React, { useRef, useState } from "react";
 import { useForm } from "react-hook-form";
 import { FaRegEyeSlash } from "react-icons/fa6";
 import { FaRegEye } from "react-icons/fa6";
@@ -8,18 +9,35 @@ import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
 const Register = () => {
-
-
   const {
     register,
     handleSubmit,
     formState: { errors },
-    reset
+    reset,
   } = useForm();
   const navigate = useNavigate();
   const [registerError, setError] = useState("");
 
-  const [userRegister , {isLoading}] = useUserRegisterMutation();
+  const [userRegister, { isLoading:registeLoading }] = useUserRegisterMutation();
+  const fileref = useRef();
+  const [image, setImage] = useState('');
+
+  const [imageUploade,{isLoading: imageLoading}] = useImageUploadeMutation()
+
+  const handleImageChange = async (e) => {
+    const file = e.target.files[0];
+    const formData = new FormData();
+    formData.append("image", file);
+
+    try {
+      const res = await imageUploade(formData).unwrap();
+      setImage(res);
+      // (res);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+    
 
   const onSubmit = async (data) => {
     if (data.password !== data.confirmPassword) {
@@ -31,25 +49,25 @@ const Register = () => {
       name: data.name,
       email: data.email,
       password: data.password,
+      avatar: image?.data
     };
 
     try {
       const response = await userRegister(register).unwrap();
-      if(response.success){
+      console.log(response);
+      
+      if (response.success) {
         toast.success("Register success");
         navigate("/login");
-        reset()
+        reset();
       }
     } catch (error) {
-     setError(error?.data?.message);
+      setError(error?.data?.message);
     }
-
-
   };
 
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
-
 
   return (
     <section className="w-full container mx-auto px-2 mt-16">
@@ -58,7 +76,35 @@ const Register = () => {
         <p>Welcome to Shoppire</p>
 
         <form className="grid gap-4 mt-6" onSubmit={handleSubmit(onSubmit)}>
-
+          <div className="">
+            {/* Profile Image */}
+            <div className="flex flex-col items-center mb-4">
+              <div className=" flex">
+                {imageLoading ? (
+                  <ImageLoading />
+                ) : (
+                  <img
+                    className=" border border-black bg-slate-100 h-24 w-24 hover:bg-indigo-50 transition duration-300  rounded-full"
+                    src={
+                      image?.data ||
+                      "https://www.shutterstock.com/image-vector/upload-icon-vector-web-computer-260nw-1924011959.jpg"
+                    }
+                    onClick={() => fileref.current.click()}
+                    alt=""
+                  />
+                )}
+              </div>
+            </div>
+            <input
+                name="profileImage"
+                type="file"
+                accept="image/*"
+                ref={fileref}
+                hidden
+                onChange={handleImageChange}
+                className="text-sm text-gray-500"
+              />
+          </div>
           <div className="grid gap-1">
             <label htmlFor="name">Name </label>
             <input
@@ -148,7 +194,7 @@ const Register = () => {
             className={`bg-slate-900 text-white py-2 rounded font-semibold my-3 tracking-wide`}
           >
             {/* Register */}
-           {isLoading ? <p>Loading...</p> :'Register'}
+            {registeLoading ? <p>Loading...</p> : "Register"}
           </button>
         </form>
 
