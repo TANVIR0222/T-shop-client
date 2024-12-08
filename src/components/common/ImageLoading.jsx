945240440cc8f134a668b332f7f7098c64df4d6b
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-12-08 17:13:49 +0600

    Image Loading page

---
+++ src/components/common/ImageLoading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ImageLoading = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <div className="w-12 h-12 border-b-2 border-green-400 border-solid rounded-full animate-spin"></div>
+    </div>
+  );
+};
+
+export default ImageLoading;
