55836de3ab0db29789a427577948d893958c8a2e
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-12-08 12:47:06 +0600

    Title page

--- src/components/common/Title.jsx
+++ src/components/common/Title.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Title = ({title,titleStyle}) => {
     return (
-        <div className={`${titleStyle} p-20`}>
+        <div className={`${titleStyle} p-10`}>
             <span className='h2 capitalize relative after:w-2/3 after:h-1 after:bg-secondary after:absolute after:-bottom-1 after:right-0 after:rounded' >{title}</span>
         </div>
     );
