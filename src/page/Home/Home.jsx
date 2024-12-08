4528de2f4b2dc22d46d3e2bbb0fdfbeac543b762
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-12-08 12:47:41 +0600

    Home page

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -1,13 +1,19 @@
+import Blogs from "./Blogs";
+import DealsSections from "./DealsSections";
 import Feature from "./Feature";
 import Hero from "./Hero";
 import ShowProduct from "./ShowProduct";
+import SubHero from "./SubHero";
 
 const Home = () => {
     return (
         <div>
            <Hero /> 
+           <SubHero />
            <ShowProduct />
+           <DealsSections />
            <Feature />
+           <Blogs />
         </div>
     );
 };
