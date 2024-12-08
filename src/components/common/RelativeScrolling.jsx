5d4daa57d04f2fffada68d67af0704b6b30a3da2
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-12-08 17:14:54 +0600

    Relative Scrolling view page

--- src/components/common/RelativeScrolling.jsx
+++ src/components/common/RelativeScrolling.jsx
@@ -5,20 +5,6 @@ import { Link } from "react-router-dom";
 const RelativeScrolling = ({ id }) => {
   const { data: relatedProduct } = useRelatedProductQuery(id);
 
-  const works = [
-    {
-      artist: "Ornella Binni",
-      art: "https://images.unsplash.com/photo-1465869185982-5a1a7522cbcb?auto=format&fit=crop&w=300&q=80",
-    },
-    {
-      artist: "Tom Byrom",
-      art: "https://images.unsplash.com/photo-1548516173-3cabfa4607e9?auto=format&fit=crop&w=300&q=80",
-    },
-    {
-      artist: "Vladimir Malyavko",
-      art: "https://images.unsplash.com/photo-1494337480532-3725c85fd2ab?auto=format&fit=crop&w=300&q=80",
-    },
-  ];
   return (
     <div>
       <ScrollArea className="w-full whitespace-nowrap rounded-md border">
